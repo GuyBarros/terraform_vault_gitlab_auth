16d9e544e3b4d963c96029f776a24975c1060f2d
Guy Barros <38210175+GuyBarros@users.noreply.github.com>
2022-05-05 15:56:59 +0100

    Update vault.tf

--- vault.tf
+++ vault.tf
@@ -1,10 +1,10 @@
 resource "vault_jwt_auth_backend" "gitlab" {
-description = "JWT auth backend for Gitlab-CI pipeline"
-path = "gitlab_jwt"
-jwks_url = "https://gitlab.com/-/jwks"
-bound_issuer = "gitlab.com"
-default_role = "generic"
-tune {
+  description  = "JWT auth backend for Gitlab-CI pipeline"
+  path         = "gitlab_jwt"
+  jwks_url     = "https://gitlab.com/-/jwks"
+  bound_issuer = "gitlab.com"
+  default_role = "generic"
+  tune {
     listing_visibility = "unauth"
     default_lease_ttl  = var.gitlab_default_lease_ttl
     max_lease_ttl      = var.gitlab_max_lease_ttl
@@ -12,53 +12,85 @@ tune {
   }
 }
 
-resource "vault_jwt_auth_backend_role" "pipeline"{
-    backend = vault_jwt_auth_backend.gitlab.path
-    role_type = "jwt"
+resource "vault_policy" "pipeline-policy" {
+  name = "pipeline-policy"
 
-    role_name = "app-pipeline"
-    token_policies = ["default", "hcp-root"]
-
-    bound_claims = {
-        project_id = data.gitlab_project.vault.id
-        ref        = "main"
-        ref_type   = "branch"
-    }
-
-    user_claim = "environment"
-
-    claim_mappings = {
-        "project_id" = "project_id"
-        "namespace_id" = "namespace_id"
-        "namespace_path" = "namespace_path"
-        "project_id" = "project_id"
-        "project_path" = "project_path"
-        "user_id" = "user_id"
-        "user_login" = "user_login"
-        "user_email" = "user_email"
-        "pipeline_id" = "pipeline_id"
-        "pipeline_source" = "pipeline_source"
-        "job_id" = "job_id"
-        "ref" = "ref"
-        "ref_type" = "ref_type"
-        "ref_protected" = "ref_protected"
-        "environment" = "environment"
-        "environment_protected" = "environment_protected"
-    }
+  policy = <<EOT
+path "kv/test" {
+  capabilities = ["read"]
 }
 
-resource "vault_jwt_auth_backend_role" "generic"{
-    backend = vault_jwt_auth_backend.gitlab.path
-    role_type = "jwt"
+path "kv/data/test" {
+  capabilities = ["read"]
+}
+EOT
+}
 
-    role_name = "generic"
-    token_policies = ["default", "hcp-root"]
+resource "vault_jwt_auth_backend_role" "pipeline" {
+  backend   = vault_jwt_auth_backend.gitlab.path
+  role_type = "jwt"
 
-    bound_claims = {
-        project_id = data.gitlab_project.vault.id
-        ref        = "main"
-        ref_type   = "branch"
-    }
+  role_name      = "app-pipeline"
+  token_policies = ["default", "pipeline-policy"]
 
-    user_claim = "user_email"
+  bound_claims = {
+    project_id = data.gitlab_project.vault.id
+    ref        = "main"
+    ref_type   = "branch"
+  }
+
+  user_claim = "environment"
+
+  claim_mappings = {
+    "project_id"            = "project_id"
+    "namespace_id"          = "namespace_id"
+    "namespace_path"        = "namespace_path"
+    "project_id"            = "project_id"
+    "project_path"          = "project_path"
+    "user_id"               = "user_id"
+    "user_login"            = "user_login"
+    "user_email"            = "user_email"
+    "pipeline_id"           = "pipeline_id"
+    "pipeline_source"       = "pipeline_source"
+    "job_id"                = "job_id"
+    "ref"                   = "ref"
+    "ref_type"              = "ref_type"
+    "ref_protected"         = "ref_protected"
+    "environment"           = "environment"
+    "environment_protected" = "environment_protected"
+  }
+}
+
+resource "vault_jwt_auth_backend_role" "generic" {
+  backend   = vault_jwt_auth_backend.gitlab.path
+  role_type = "jwt"
+
+  role_name      = "generic"
+  token_policies = ["default", "pipeline-policy"]
+
+  bound_claims = {
+    project_id = data.gitlab_project.vault.id
+    ref        = "main"
+    ref_type   = "branch"
+  }
+
+  user_claim = "user_email"
+}
+
+resource "vault_mount" "secret-mount" {
+  path        = "kv"
+  type        = "kv-v2"
+  description = "KV Secrets engine"
+}
+
+resource "vault_generic_secret" "lob-secret" {
+  path = "kv/test"
+
+  data_json = <<EOT
+{
+    "foo": "This is my super secure secret!"
+}
+EOT
+
+  depends_on = [vault_mount.secret-mount]
 }
